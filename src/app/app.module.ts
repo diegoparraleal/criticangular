4192510c1588509770720a0dddfe43be300e9584
Diego Parra <diego.parra.leal@gmail.com>
2021-03-11 17:11:09 -0500

    Removed client id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -85,7 +85,7 @@ import { ConfirmationComponent } from './confirmation/confirmation.component';
           {
             id: GoogleLoginProvider.PROVIDER_ID,
             provider: new GoogleLoginProvider(
-              '910891705833-ch13cbd7je16n0uscshe6n90eu3k8098.apps.googleusercontent.com'
+              '<PLEASE ADD YOUR CLIENT ID HERE>'
             )
           }
         ]
